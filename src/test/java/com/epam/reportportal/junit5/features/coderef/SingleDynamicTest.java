881bb0d83a114cf1828a5e288e8d725962a0d45b
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2020-02-04 19:37:26 +0300

    Another unittest fix try: split tests into two classes

--- src/test/java/com/epam/reportportal/junit5/features/coderef/SingleDynamicTest.java
+++ src/test/java/com/epam/reportportal/junit5/features/coderef/SingleDynamicTest.java
@@ -1,6 +1,6 @@
 package com.epam.reportportal.junit5.features.coderef;
 
-import com.epam.reportportal.junit5.CodeReferenceTests;
+import com.epam.reportportal.junit5.DynamicCodeReferenceTest;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,7 +10,7 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
-@ExtendWith(CodeReferenceTests.DynamicCodeReferenceTestExtension.class)
+@ExtendWith(DynamicCodeReferenceTest.DynamicCodeReferenceTestExtension.class)
 public class SingleDynamicTest {
 
 	public static final String TEST_CASE_DISPLAY_NAME = UUID.randomUUID().toString();
--- src/test/java/com/epam/reportportal/junit5/features/coderef/SingleTest.java
+++ src/test/java/com/epam/reportportal/junit5/features/coderef/SingleTest.java
@@ -1,11 +1,11 @@
 package com.epam.reportportal.junit5.features.coderef;
 
-import com.epam.reportportal.junit5.CodeReferenceTests;
+import com.epam.reportportal.junit5.StaticCodeReferenceTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-@ExtendWith(CodeReferenceTests.CodeReferenceTestExtension.class)
+@ExtendWith(StaticCodeReferenceTest.CodeReferenceTestExtension.class)
 public class SingleTest {
 
 	public static final String TEST_CASE_DISPLAY_NAME = "0c6b91e-623d-498d-a56d-fae1f2bbcbe4";

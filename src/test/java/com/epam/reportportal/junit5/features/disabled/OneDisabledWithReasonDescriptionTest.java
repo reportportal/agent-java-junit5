33466bb1821ad6ae7b1dea138ad84982ea52d781
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2024-04-09 16:21:35 +0300

    Client version update, do not hide test description when test is disabled

--- src/test/java/com/epam/reportportal/junit5/features/disabled/OneDisabledWithReasonDescriptionTest.java
+++ src/test/java/com/epam/reportportal/junit5/features/disabled/OneDisabledWithReasonDescriptionTest.java
@@ -1,16 +1,18 @@
 package com.epam.reportportal.junit5.features.disabled;
 
+import com.epam.reportportal.annotations.Description;
 import com.epam.reportportal.junit5.DisabledTestTest;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(DisabledTestTest.DisabledTestExtension.class)
-public class OneDisabledTestWithReason {
-
+public class OneDisabledWithReasonDescriptionTest {
+	public static final String TEST_DESCRIPTION_METHOD = "My test description on the method";
 	public static final String REASON = "My reason to disable test";
 
 	@Disabled(REASON)
+	@Description(TEST_DESCRIPTION_METHOD)
 	@Test
 	void disabledTest() {
 		System.out.println("disabled");

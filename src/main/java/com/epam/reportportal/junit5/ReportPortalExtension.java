45d1356296c6bfed9c3e6df0bd819e80d18f97b5
admitrov <admitrov@playtika.com>
2025-05-05 15:55:01 +0100

    Javax dependency in new spring boot #137

--- src/main/java/com/epam/reportportal/junit5/ReportPortalExtension.java
+++ src/main/java/com/epam/reportportal/junit5/ReportPortalExtension.java
@@ -42,8 +42,8 @@ import org.opentest4j.TestAbortedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
 import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
